731ba7f3508df588f2576b391d9cc730ac50e5e8
Frederik Held <konten@frederikheld.de>
2019-03-17 21:42:02 +0100

    Updates example to render stories
    Links don't work here!

--- example/README.md
+++ example/README.md
@@ -1,9 +1,23 @@
 # usm.io example
 
+This example shows how usm.io can be used.
+
+## Run the example
+
 Run
 
     $ node generator.js
 
-to render `input/usm-example.json` into `web/usm-example.html`
+to render `input/usm-example.json` into `web/usm-example.html`. Open the html file in your browser to see the result.
 
-You are free to use `web/scripts.js` and `web/styles.css` to get your own usm.io project started.
+Edit the json file, run the generatore again and refresh your browser to see how changes in the json file are rendered to html.
+
+## Re-use
+
+`usm.io` will only render the html file. It can link styles and scripts in the rendered html, but you need to provide your own styles and scripts to bring it to life.
+
+To get your project started quickly, I recommend to to use `web/scripts.js` and `web/styles.css` from this example.
+
+## Sources
+
+This example implements Jeff Patton's User Story Mapping example "Film Finder". [You'll find the template files here]( https://www.dropbox.com/sh/naxyjgn7wwvtmmz/AAAEGR4Lav8rz2eTbyAJGgUia?dl=0).
--- example/generator.js
+++ example/generator.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const fs = require('fs').promises
+const fsExtra = require('fs-extra')
 const path = require('path')
 
 const Usm = require('../usm/usm')
@@ -20,7 +21,28 @@ let main = async function () {
     //       So they need to be relative to the output file!
     const usm = new Usm(usmJson)
 
+    // render usm:
     const usmHtml = usm.render(options)
+
+    // Prepare output directory for packages:
+    const outputPackagesPath = path.join(__dirname, 'web', 'packages')
+    try {
+        let stat = await fs.stat(outputPackagesPath)
+        if (stat.isDirectory()) {
+            await fsExtra.remove(outputPackagesPath)
+        } else {
+            throw new Error('ERROR: "' + outputPackagesPath + '" is not a directory!')
+        }
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+        } else {
+            throw err
+        }
+    }
+    await fs.mkdir(outputPackagesPath)
+
+    // render packages:
+    usm.renderPackages(outputPackagesPath, path.join(__dirname, 'input', 'packages'))
 
     await fs.writeFile(
         path.join('web', 'usm-example.html'),
