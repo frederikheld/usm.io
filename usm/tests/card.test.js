0838f47c3d42dc7fb5156f3eeb08d856883d10d7
Frederik Held <konten@frederikheld.de>
2019-04-16 17:09:16 +0200

    Cleans up tests for card.js
    - re-structures and re-phrases them to be read as documentation easier
    - re-structures mocks for those tests to make them easier to spot (will help users to use them as documentation as well)

--- usm/tests/card.test.js
+++ usm/tests/card.test.js
@@ -14,71 +14,71 @@ const Card = require('../card')
 
 describe('card', function () {
     describe('the constructor Card()', function () {
-        context('json object passed', function () {
-            it('accepts a json object', function () {
-                expect(function () {
-                    new Card({})
-                }).to.not.throw()
-            })
+        it('accepts a json object', function () {
+            expect(function () {
+                new Card({})
+            }).to.not.throw()
         })
 
-        context('invalid data type passed', function () {
-            it('throws a TypeError, if passed data is not an object', function () {
-                expect(function () {
-                    new Card(5)
-                }).to.throw(TypeError)
+        it('throws a TypeError, if an invalid data type is passed', function () {
+            expect(function () {
+                new Card(5)
+            }).to.throw(TypeError)
 
-                expect(function () {
-                    new Card([])
-                }).to.throw(TypeError)
-            })
+            expect(function () {
+                new Card('a string')
+            }).to.throw(TypeError)
 
-            it('throws a ReferenceError, if no data is passed at all', function () {
-                expect(function () {
-                    new Card()
-                }).to.throw(ReferenceError)
-            })
+            expect(function () {
+                new Card([])
+            }).to.throw(TypeError)
+        })
+
+        it('throws a ReferenceError, if no data is passed at all', function () {
+            expect(function () {
+                new Card()
+            }).to.throw(ReferenceError)
         })
     })
 
     describe('Card.prototype.render()', function () {
-        // context('this.jsonData is invalid', function () {
-
-        // })
-
-        context('this.jsonData is valid', function () {
-            it('renders an empty Card container', async function () {
-                const rawCard = await fs.readFile(path.join(__dirname, 'mock-data', 'mock-card-empty.json'))
+        context('valid data was passed with the constructor', function () {
+            it('renders an empty Card container if the object is empty', async function () {
+                const rawCard = await fs.readFile(path.join(__dirname, 'mock-data', 'card', 'mock-card-empty.json'))
                 const jsonCard = JSON.parse(rawCard)
                 const card = new Card(jsonCard)
                 let htmlRendered = card.render()
 
-                let htmlExpected = await fs.readFile(path.join(__dirname, 'mock-data', 'mock-card-empty.html'), 'utf8')
+                let htmlExpected = await fs.readFile(path.join(__dirname, 'mock-data', 'card', 'mock-card-empty.html'), 'utf8')
 
                 helpers.stripWhitespaces(htmlRendered).should.equal(helpers.stripWhitespaces(htmlExpected))
             })
 
-            it('renders title field, if defined', async function () {
-                const rawCard = await fs.readFile(path.join(__dirname, 'mock-data', 'mock-card-title-only.json'))
+            it('renders the title field, if defined in the object', async function () {
+                const rawCard = await fs.readFile(path.join(__dirname, 'mock-data', 'card', 'mock-card-title-only.json'))
                 const jsonCard = JSON.parse(rawCard)
                 const card = new Card(jsonCard)
                 let htmlRendered = card.render()
 
-                let htmlExpected = await fs.readFile(path.join(__dirname, 'mock-data', 'mock-card-title-only.html'), 'utf8')
+                let htmlExpected = await fs.readFile(path.join(__dirname, 'mock-data', 'card', 'mock-card-title-only.html'), 'utf8')
 
                 helpers.stripWhitespaces(htmlRendered).should.equal(helpers.stripWhitespaces(htmlExpected))
             })
 
-            it('renders the description, if defined', async function () {
-                const rawCard = await fs.readFile(path.join(__dirname, 'mock-data', 'mock-card-description-only.json'))
+            it('renders the description, if defined in the object', async function () {
+                const rawCard = await fs.readFile(path.join(__dirname, 'mock-data', 'card', 'mock-card-description-only.json'))
                 const jsonCard = JSON.parse(rawCard)
                 const card = new Card(jsonCard)
                 let htmlRendered = card.render()
 
-                let htmlExpected = await fs.readFile(path.join(__dirname, 'mock-data', 'mock-card-description-only.html'), 'utf8')
+                let htmlExpected = await fs.readFile(path.join(__dirname, 'mock-data', 'card', 'mock-card-description-only.html'), 'utf8')
 
                 helpers.stripWhitespaces(htmlRendered).should.equal(helpers.stripWhitespaces(htmlExpected))
             })
         })
+
+        // context('invalid data was passed with the constructor', function () {
+
+        // })
     })
 })
