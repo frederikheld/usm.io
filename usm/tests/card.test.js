0f6618a062ae5686fef02c211d4fc0c6e52b99c4
Frederik Held <konten@frederikheld.de>
2019-04-16 19:43:29 +0200

    Replaces simple mock files by inline mocks for better readability
    Tests are easier to read if all mocked information is in place instead of having it in separate files. On the other hand, long mocked inputs and outputs can make the test lengthy and unreadable. This replaces all mocked files that contain objects with just one key with inline objects. Longer mock files are kept.

--- usm/tests/card.test.js
+++ usm/tests/card.test.js
@@ -44,33 +44,31 @@ describe('card', function () {
     describe('Card.prototype.render()', function () {
         context('valid data was passed with the constructor', function () {
             it('renders an empty Card container if the object is empty', async function () {
-                const rawCard = await fs.readFile(path.join(__dirname, 'mock-data', 'card', 'mock-card-empty.json'))
-                const jsonCard = JSON.parse(rawCard)
-                const card = new Card(jsonCard)
-                let htmlRendered = card.render()
+                const card = new Card({})
 
+                let htmlRendered = card.render()
                 let htmlExpected = await fs.readFile(path.join(__dirname, 'mock-data', 'card', 'mock-card-empty.html'), 'utf8')
 
                 helpers.stripWhitespaces(htmlRendered).should.equal(helpers.stripWhitespaces(htmlExpected))
             })
 
             it('renders the title field, if defined in the object', async function () {
-                const rawCard = await fs.readFile(path.join(__dirname, 'mock-data', 'card', 'mock-card-title-only.json'))
-                const jsonCard = JSON.parse(rawCard)
-                const card = new Card(jsonCard)
-                let htmlRendered = card.render()
+                const card = new Card({
+                    title: 'This is the title'
+                })
 
+                let htmlRendered = card.render()
                 let htmlExpected = await fs.readFile(path.join(__dirname, 'mock-data', 'card', 'mock-card-title-only.html'), 'utf8')
 
                 helpers.stripWhitespaces(htmlRendered).should.equal(helpers.stripWhitespaces(htmlExpected))
             })
 
             it('renders the description, if defined in the object', async function () {
-                const rawCard = await fs.readFile(path.join(__dirname, 'mock-data', 'card', 'mock-card-description-only.json'))
-                const jsonCard = JSON.parse(rawCard)
-                const card = new Card(jsonCard)
-                let htmlRendered = card.render()
+                const card = new Card({
+                    description: 'AS developer\nI WANT TO have the description field rendered nicely\nSO THAT I can sleep well'
+                })
 
+                let htmlRendered = card.render()
                 let htmlExpected = await fs.readFile(path.join(__dirname, 'mock-data', 'card', 'mock-card-description-only.html'), 'utf8')
 
                 helpers.stripWhitespaces(htmlRendered).should.equal(helpers.stripWhitespaces(htmlExpected))
