29780a84f7b76fa815cbbd8778ae37a33c1f4fc1
Frederik Held <konten@frederikheld.de>
2019-04-29 22:39:22 +0200

    Fixes tests for Card

--- usm/tests/card.test.js
+++ usm/tests/card.test.js
@@ -47,20 +47,6 @@ describe('card', function () {
                     new Card(undefined, {})
                 }).to.throw(ReferenceError)
             })
-
-            /**
-             * Link modes:
-             * a) none
-             * <!-- // might come later
-             * b) json file linked
-             *      * merges fields from linked json file with object's own fields
-             * //-->
-             * c) package folder linked
-             *      * merges fields from _card.json_ in linked directory with object's own fields
-             *      * renders button that links _index.html_ in the package's outputDir
-             * d) error (link given that doesn't match one of the above criteria)
-             *      * throws an error
-             */
         })
 
         describe('the second parameter "context"', function () {
@@ -90,47 +76,47 @@ describe('card', function () {
                 }).to.throw(ReferenceError)
             })
         })
-    })
 
-    describe('Card.prototype.load()', function () {
-        context('the object has a field "package"', async function () {
-            // it('throws an ReferenceError if "package.inputDir" is not given', async function () {
+        context('the object has a field "package"', function () {
+            // it('throws an ReferenceError if "package.inputDir" is not given', function () {
             //     const card = new Card({
             //         package: { }
             //     }, {})
 
-            //     await card.load().should.be.rejectedWith(ReferenceError, '"package" given but "package.inputDir" missing!')
+            //     card.load().should.be.rejectedWith(ReferenceError, '"package" given but "package.inputDir" missing!')
             // })
 
-            it('loads the json object from "card.json" in the directory with the given package-id in "inputDir/cards"', async function () {
+            it('loads the json object from "card.json" in the directory with the given package-id in "inputDir/cards"', function () {
                 const jsonCard = {
                     package: 'card-package'
                 }
 
-                const card = new Card(jsonCard, cardContext)
-                await card.load().should.be.fulfilled
+                let card
+                expect(function () {
+                    card = new Card(jsonCard, cardContext)
+                }).to.not.throw()
 
                 card.get('title').should.equal('Card description from package')
                 card.get('description').should.equal('This description comes from the card in the package')
             })
 
-            it('rejects with a ReferenceError if "card.json" wasn\'t found in the package\'s directory', async function () {
+            it('throws a ReferenceError if "card.json" wasn\'t found in the package\'s directory', function () {
                 const jsonCard = {
                     package: 'invalid-card-package'
                 }
-                const card = new Card(jsonCard, cardContext)
-                await card.load().should.be.rejectedWith(ReferenceError, 'Could not read from "card.json" in package "' + jsonCard.package + '"')
+
+                expect(function () {
+                    new Card(jsonCard, cardContext)
+                }).throw(ReferenceError, 'Could not read from "card.json" in package "' + jsonCard.package + '"')
             })
 
-            it('overwrites fields from "card.json" with fields from the object', async function () {
+            it('overwrites fields from "card.json" with fields from the object', function () {
                 const jsonCard = {
                     title: 'Title from the object',
                     package: 'card-package'
                 }
 
                 const card = new Card(jsonCard, cardContext)
-
-                await card.load()
 
                 card.get('title').should.equal('Title from the object')
                 card.get('description').should.equal('This description comes from the card in the package')
@@ -140,16 +126,6 @@ describe('card', function () {
 
     describe('Card.prototype.render()', function () {
         context('valid data was passed to the constructor', function () {
-            it('throws an error if the jsonCard contains a package link and then Card.render() is called before Card.load() was called', function () {
-                const card = new Card({
-                    package: 'card-package'
-                }, cardContext)
-
-                expect(function () {
-                    card.render()
-                }).to.throw(Error)
-            })
-
             it('renders an empty Card container, if the object is empty', async function () {
                 const card = new Card({}, {})
 
@@ -201,22 +177,10 @@ describe('card', function () {
 
                     const card = new Card(jsonCard, cardContext)
 
-                    await card.load()
-
                     let htmlRendered = card.render()
-                    let htmlSnippetExpected = '<button onclick="window.location.href=\'' + cardContext.outputDir + '/cards/' + jsonCard.package + '/index.html\'">Open Package</button>'
+                    let htmlSnippetExpected = '<button class="open-package" onclick="window.location.href=\'' + cardContext.outputDir + '/cards/' + jsonCard.package + '/index.html\'">Open Package</button>'
 
                     helpers.stripWhitespaces(htmlRendered).includes(helpers.stripWhitespaces(htmlSnippetExpected)).should.be.true
-                })
-
-                it('throws an error if Card.load() wasn\'t called before Card.render() was called', async function () {
-                    const card = new Card({
-                        package: 'card-package'
-                    }, {})
-
-                    expect(function () {
-                        card.render()
-                    }).to.throw(Error, 'Your json card description contains a link to a package that needs to be loaded before it can be rendered. Please run Card.load() before you run Card.render()!')
                 })
             })
 
@@ -227,7 +191,7 @@ describe('card', function () {
                     }, {})
 
                     let htmlRendered = card.render()
-                    let htmlSnippetNotExpected1 = '<button onclick="window.location.href=\'' + cardContext.outputDir + '/cards/'
+                    let htmlSnippetNotExpected1 = '<button class="open-package" onclick="window.location.href=\'' + cardContext.outputDir + '/cards/'
                     let htmlSnippetNotExpected2 = '/index.html\'">Open Package</button>'
 
                     helpers.stripWhitespaces(htmlRendered).includes(helpers.stripWhitespaces(htmlSnippetNotExpected1)).should.be.false
