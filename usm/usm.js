a0069c2f04420dc1871e7a7b8032879929ee757f
Frederik Held <konten@frederikheld.de>
2019-05-08 00:08:09 +0200

    usm will be capable to render releases
    CSS implementation as previously shown in the experiment

--- usm/usm.js
+++ usm/usm.js
@@ -92,6 +92,10 @@ Usm.prototype.renderMap = async function (config) {
         }
     }
 
+    result += '\n    <style>'
+    result += __generateReleasesCSS(this.jsonUsm, 8)
+    result += '\n    </style>'
+
     result += '\n</head>\n'
     result += '\n<body>'
 
@@ -120,4 +124,84 @@ Usm.prototype.renderMap = async function (config) {
     result += '\n</html>'
 
     await fs.writeFile(path.join(this.context.outputDir, 'index.html'), result)
+}
+
+function __generateReleasesCSS (jsonUsm, indentBlanks) {
+    const releases = jsonUsm.releases
+    const activities = jsonUsm.activities
+
+    // extract release keys:
+
+    let releaseKeys = []
+    for (let i = 0; i < releases.length; i++) {
+        releaseKeys.push(releases[i].key)
+    }
+
+    // count max number of cards per release within a step:
+
+    let maxCardsInRelease = {}
+    for (let i = 0; i < releaseKeys.length; i++) {
+        maxCardsInRelease[releaseKeys[i]] = 0
+    }
+
+    for (let aI = 0; aI < activities.length; aI++) {
+        for (let sI = 0; sI < activities[aI].steps.length; sI++) {
+            // prepare collection:
+            let maxCardsInReleaseInStep = {}
+            for (let i = 0; i < releaseKeys.length; i++) {
+                maxCardsInReleaseInStep[releaseKeys[i]] = 0
+            }
+
+            // count occurences in step:
+            for (let storiesI = 0; storiesI < activities[aI].steps[sI].stories.length; storiesI++) {
+                if (activities[aI].steps[sI].stories[storiesI].inRelease) {
+                    maxCardsInReleaseInStep[activities[aI].steps[sI].stories[storiesI].inRelease]++
+                }
+            }
+
+            // update global collection:
+            for (let key in maxCardsInReleaseInStep) {
+                if (maxCardsInReleaseInStep[key] > maxCardsInRelease[key]) {
+                    maxCardsInRelease[key] = maxCardsInReleaseInStep[key]
+                }
+            }
+        }
+    }
+
+    // stories container:
+
+    const blanks = new Array(indentBlanks + 1).join(' ')
+
+    let result = '\n' + blanks + '.stories {'
+    result += '\n' + blanks + '    grid-template-rows:'
+    for (let i = 0; i < releases.length; i++) {
+        result += ' calc(6.8rem * ' + (maxCardsInRelease[releaseKeys[i]]) + ')'
+    }
+    result += '\n' + blanks + '}'
+    result += '\n'
+
+    // position release containers
+
+    for (let i = 0; i < releases.length; i++) {
+        result += '\n' + blanks + '.release-' + releases[i].key + ' {'
+        result += '\n' + blanks + '    grid-row-start: ' + (i + 1) + ';'
+        result += '\n' + blanks + '    grid-row-end: ' + (i + 2) + ';'
+        result += '\n' + blanks + '}'
+    }
+    result += '\n'
+
+    // position release names:
+
+    for (let i = 0; i < releases.length; i++) {
+        result += '\n' + blanks + '.release-' + releases[i].key + '::before {'
+        result += '\n' + blanks + '    content: "' + releases[i].title + '";'
+        result += '\n' + blanks + '    margin-top: calc(6.8rem * ' + (maxCardsInRelease[releaseKeys[i]]) + '/2 - 0.4rem);'
+        result += '\n' + blanks + '    width: calc(6.8rem * ' + (maxCardsInRelease[releaseKeys[i]]) + ');'
+        result += '\n' + blanks + '    left: calc(-3.42rem * ' + (maxCardsInRelease[releaseKeys[i]]) + ' + 0.8rem);'
+        result += '\n' + blanks + '}'
+    }
+
+    // return result:
+
+    return result
 }
